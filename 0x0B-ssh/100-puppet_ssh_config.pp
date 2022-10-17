808d5135e345ca819c62a4b2dd6898d1c334e6f7
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-10-17 05:07:22 +0100

    New update in 0x0B ssh task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
-# connect to a server Execute a command
-
-exec { 'echo "PasswordAuthentication no\n IdentifyFile ~/.ssh/school" >> /etc/ssh/ssh_config';
-path => '/bin/'
-}
+# connect to a server Execute a command 
+  
+ exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config': 
+         path    => '/bin/' 
+ }
